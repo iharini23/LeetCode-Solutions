792e26ccbdd33c92b8607a6724dd81d5769d7eab
iharini23 <iharini3123@gmail.com>
2026-02-09 09:39:31 +0530

    Time: 0 ms (100%), Space: 47.9 MB (97.97%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void reverseString(char[] s) {
+        int left=0;
+        int right=s.length-1;
+        int n=s.length;
+        while(left<right){
+            char temp=s[left];
+            s[left]=s[right];
+            s[right]=temp;
+            left++;
+            right--;
+        }
+        
+    }
+}
